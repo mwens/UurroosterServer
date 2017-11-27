3c08e3a846889cea70deec681a88e55e274e54b5
Witmoca <personal@witmoca.be>
2017-11-27 14:30:42 +0100

    Oracle vervangen door Derby, statements aangepast

--- JenteenMatthias/UurroosterServer/creatie.sql
+++ JenteenMatthias/UurroosterServer/creatie.sql
@@ -7,31 +7,34 @@
  * Author:  Jente
  * Created: 21-nov-2017
  */
-
+drop table URS_StudentRelatie;
+drop table URS_Student;
+drop table URS_Groep;
+drop table URS_Docent;
 
 create table URS_Docent(
         id      int,
-        naam    varchar2(32)    not null,
-        ww      varchar2(32)    not null,
+        naam    varchar(32)    not null,
+        ww      varchar(32)    not null,
         primary key(id)
         );
 
 create table URS_Groep(
         id      int,
-        naam    varchar2(32)    not null,
+        naam    varchar(32)    not null,
         status  int             not null,
         primary key(id)
         );
 
 create table URS_Student(
         id      int,
-        naam    varchar2(32)    not null,
-        ww      varchar2(32)    not null,
+        naam    varchar(32)    not null,
+        ww      varchar(32)    not null,
         /* 0 = OPEN, 1 = GESLOTEN */
         status  int             not null,
-        groep   int             null,
+        groep   int,
         primary key(id),
-        foreign key(groep) references URSGroep
+        foreign key(groep) references URS_Groep
         );
         
 create table URS_StudentRelatie(
--- JenteenMatthias/UurroosterServer/inserts.sql
+++ JenteenMatthias/UurroosterServer/inserts.sql
@@ -8,27 +8,27 @@
  * Created: 21-nov-2017
  */
 
-INSERT into URS_Student VALUES (0,"Matthias Wens","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (1,"Jente Heremans","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (2,"Jeroentje Streulens","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (3,"Wouter Mauriën","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (4,"Wouter Raes","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (5,"Pieterjan Peetermans","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (6,"Nele Annaert","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (7,"Jelle Meus","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (8,"Jari Rooman","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (9,"Toon Blommaerts","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (10,"Wouter Symons","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (11,"Nick Todts","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (12,"Kevin Todts","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (13,"Kristof De Ridder","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (14,"Lena Bastenie","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (15,"Bruno Horemans","PassW0rd",0,NULL);
-INSERT into URS_Student VALUES (16,"Jan van der Veken","PassW0rd",0,NULL);
+INSERT into URS_STUDENT VALUES (0,'Matthias Wens','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (1,'Jente Heremans','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (2,'Jeroentje Streulens','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (3,'Wouter Mauriën','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (4,'Wouter Raes','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (5,'Pieterjan Peetermans','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (6,'Nele Annaert','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (7,'Jelle Meus','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (8,'Jari Rooman','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (9,'Toon Blommaerts','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (10,'Wouter Symons','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (11,'Nick Todts','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (12,'Kevin Todts','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (13,'Kristof De Ridder','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (14,'Lena Bastenie','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (15,'Bruno Horemans','PassW0rd',0,NULL);
+INSERT into URS_Student VALUES (16,'Jan van der Veken','PassW0rd',0,NULL);
 
-INSERT into URS_Docent VALUES (0,"admin","admin");
-INSERT into URS_Docent VALUES (1,"Herman Crauwels","IH8Toledo");
-INSERT into URS_Docent VALUES (2,"Joost Vennekens","IH8Toledo");
+INSERT into URS_Docent VALUES (0,'admin','admin');
+INSERT into URS_Docent VALUES (1,'Herman Crauwels','IH8Toledo');
+INSERT into URS_Docent VALUES (2,'Joost Vennekens','IH8Toledo');
 
 
 
@@ -39,4 +39,4 @@ INSERT into URS_StudentRelatie VALUES (0,16,1);
 INSERT into URS_StudentRelatie VALUES (0,3,1);
 
 
-INSERT into URS_Groep VALUES (1,"Groep 1",0);
+INSERT into URS_Groep VALUES (1,'Groep 1',0);
