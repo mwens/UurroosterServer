d72e6cad83fc97902d9191f0b20a50645c75614a
mwens <matthias_wens@hotmail.com>
2017-12-02 13:14:09 +0100

    Relaties worden aan tabel toegevoegd. Ze worden er echter nog niet juist uitgehaald en afgebeeld op de student.jsp pagina.

--- JenteenMatthias/UurroosterServer/UurroosterServer-war/src/pakket/student.java
+++ JenteenMatthias/UurroosterServer/UurroosterServer-war/src/pakket/student.java
@@ -43,8 +43,18 @@ public class student extends HttpServlet {
             throws ServletException, IOException {
         int userId = commonBean.getUserId(request.getUserPrincipal().getName());
         System.out.println(studentBean.getStatus(userId));
+        HttpSession sessie = request.getSession();   
+        studentBean.setStatus(userId, 2);
+        System.out.println(studentBean.getStatus(userId));
+        List<UrsStudentrelatie> l = studentBean.getRelaties(userId);
+        System.out.println("Relaties voor:");
+        for(UrsStudentrelatie sr : l){
+            System.out.println(sr.getUrsStudentrelatiePK().getCollega() + " : " + ((sr.getRelatie() == 2) ? "Niet" : "Wel"));
+        }
         
-        HttpSession sessie = request.getSession();
+        
+        /*Vanaf hier stuk nieuwe code matthias*/
+        
         List<UrsGebruiker> studenten = commonBean.getStudentenLijst(request.getUserPrincipal().getName());
         sessie.setAttribute("studenten", studenten);
         String stage = request.getParameter("stage");
@@ -62,28 +72,16 @@ public class student extends HttpServlet {
             studentBean.setRelatie(userId,collegaId, 2);
             System.out.println("relatie toegevoegd X");
         }
-        List<UrsStudentrelatie> studentenRelaties = studentBean.getRelaties(userId); 
-        sessie.setAttribute("studentenRelaties", studentenRelaties);
         
+        /*Tot hier stuk nieuwe code matthias*/
         
-        
-        
-        
-        
-        studentBean.setStatus(userId, 2);
-        System.out.println(studentBean.getStatus(userId));
-        List<UrsStudentrelatie> l = studentBean.getRelaties(userId);
-        System.out.println("Relaties voor:");
-        for(UrsStudentrelatie sr : l){
-            System.out.println(sr.getUrsStudentrelatiePK().getCollega() + " : " + ((sr.getRelatie() == 2) ? "Niet" : "Wel"));
-        }
-        
-        studentBean.setRelatie(0, 1, 2); //Pas aan
-        studentBean.setRelatie(0, 2, 1); // Pas aan
-        studentBean.setRelatie(0, 6, 1); // Creeer
-        studentBean.setRelatie(0, 14, 0); // delete
+        //studentBean.setRelatie(0, 1, 2); //Pas aan
+        //studentBean.setRelatie(0, 2, 1); // Pas aan
+        //studentBean.setRelatie(0, 6, 1); // Creeer
+        //studentBean.setRelatie(0, 14, 0); // delete
         System.out.println("Relaties Na:");
         l = studentBean.getRelaties(userId);
+        sessie.setAttribute("studentenRelaties", l);
         for(UrsStudentrelatie sr : l){
             System.out.println(sr.getUrsStudentrelatiePK().getCollega() + " : " + ((sr.getRelatie() == 2) ? "Niet" : "Wel"));
         }
